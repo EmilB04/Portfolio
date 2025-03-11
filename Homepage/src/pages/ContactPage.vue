36b598711c4a293969229ade8b2ebaa0f0f09d53
EmilB04 <emil.berglund@live.no>
2025-03-11 01:21:44 +0100

    Started on contact page

--- Homepage/src/pages/ContactPage.vue
+++ Homepage/src/pages/ContactPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page>
-    <h1>Test</h1>
+  <q-page class="columm">
+    <header>
+      <nav class="flex justify-center">
+        <q-btn class="bg-accent q-ma-lg text-white" flat rounded label="Gå tilbake" @click="$router.push('/')" />
+      </nav>
+    </header>
+    <q-separator class="bg-white q-mx-auto" style="max-width: 1280px" />
+    <section>
+      <h1 class="text-white text-center">Emil Berglund</h1>
+    </section>
+
+
+
+    <ShootingStars />
   </q-page>
 </template>
 
-<script lang="ts">
-
+<script>
+import ShootingStars from 'src/components/ShootingStars.vue';
+export default {
+  name: 'ContactPage',
+  components: {
+    ShootingStars,
+  },
+};
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+@import 'src/css/ContactStyle.scss';
+</style>
