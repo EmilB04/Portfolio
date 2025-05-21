97cc3be1d0259e146a3680b1aa214137aafe7592
Emil Berglund <emil.berglund@live.no>
2025-05-21 17:28:19 +0200

    Update footer

--- Homepage/src/components/FooterSection.vue
+++ Homepage/src/components/FooterSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-center q-ma-sm text-white q-pa-md">&copy; 2025 Emil Berglund. Alle rettigheter reservert.</p>
+  <p class="text-center q-ma-sm text-white q-pa-md">&copy; 2025 Emil Berglund</p>
 </template>
 
 <script>
