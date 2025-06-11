69cf16926341e7a4f598e5a5b82da7b36b2a0fcb
Emil Berglund <emil.berglund@live.no>
2025-06-11 23:00:02 +0200

    Made better nav comp

--- Homepage/src/components/NavSection.vue
+++ Homepage/src/components/NavSection.vue
@@ -1,31 +1,30 @@
 <template>
-  <nav class="flex justify-between">
-    <div class="justify-start nav-links" :class="{ open: menuOpen }">
-      <q-btn flat label="Om" @click="navigate('.about-section')" />
-      <q-btn flat label="Studie" @click="navigate('.timeline-section')" />
-      <q-btn flat label="Prosjekter" @click="navigate('.projects-section')" />
-      <q-btn flat label="Kunnskap" @click="navigate('.skills-section')" />
-      <q-btn flat label="GitHub" @click="navigate('.gitHub-section')" />
-      <q-btn flat label="Tilbakemeldinger" @click="navigate('.comments-section')" />
+  <nav class="flex justify-between q-py-md">
+    <div class="justify-start nav-links flex" :class="{ open: menuOpen }">
+      <template v-if="$route.name === 'IndexPage' || $route.path === '/'">
+        <q-btn text-color="white" flat label="Om" @click="navigate('.about-section')" />
+        <q-btn text-color="white" flat label="Tidslinje" @click="navigate('.timeline-section')" />
+        <q-btn text-color="white" flat label="Prosjekter" @click="navigate('.projects-section')" />
+        <q-btn text-color="white" flat label="Kunnskap" @click="navigate('.skills-section')" />
+        <q-btn text-color="white" flat label="GitHub" @click="navigate('.gitHub-section')" />
+        <q-btn text-color="white" flat label="Tilbakemeldinger" @click="navigate('.comments-section')" />
+      </template>
+      <template v-else>
+        <q-btn text-color="white" flat icon="arrow_back" label="Tilbake" @click="$router.back()" />
+      </template>
     </div>
     <div>
-      <q-icon name="east" class="q-mx-md arrow-animate" />
-      <q-btn class="justify-end bg-accent" flat rounded label="Emil Berglund" @click="$router.push('/contact')" />
-      <q-btn
-        flat
-        dense
-        round
-        icon="menu"
-        class="hamburger"
-        @click="menuOpen = !menuOpen"
-      />
+      <template v-if="$route.name === 'IndexPage' || $route.path === '/'">
+        <q-icon name="east" class="q-mx-md arrow-animate" />
+        <q-btn class="justify-end bg-accent" flat rounded label="Emil Berglund" @click="$router.push('/contact')" />
+      </template>
+      <q-btn text-color="white" flat dense round icon="menu" class="hamburger" @click="menuOpen = !menuOpen" />
     </div>
   </nav>
 </template>
 
 <script>
 import ScrollScript from 'src/scripts/ScrollScript.js';
-
 export default {
   name: 'NavSection',
   mixins: [ScrollScript],
