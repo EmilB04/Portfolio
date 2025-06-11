30a9fafc2ba669485e965a1f81e7f0ca8274aa70
Emil Berglund <emil.berglund@live.no>
2025-06-11 22:08:07 +0200

    Add default variable for btn

--- Homepage/quasar.config.js
+++ Homepage/quasar.config.js
@@ -8,7 +8,6 @@
 // Configuration for your app
 // https://legacy-app.quasar.dev/quasar-cli-vite-v1/quasar-config-file
 
-
 const { configure } = require('quasar/wrappers');
 const path = require('path');
 
@@ -20,15 +19,10 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://legacy-app.quasar.dev/quasar-cli-vite-v1/boot-files
-    boot: [
-      'i18n',
-      'axios',
-    ],
+    boot: ['i18n', 'axios', 'defaults'],
 
     // https://legacy-app.quasar.dev/quasar-cli-vite-v1/quasar-config-file#css
-    css: [
-      'app.scss'
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -47,8 +41,8 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://legacy-app.quasar.dev/quasar-cli-vite-v1/quasar-config-file#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
-        node: 'node20'
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        node: 'node20',
       },
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
@@ -71,32 +65,39 @@ module.exports = configure(function (/* ctx */) {
       // viteVuePluginOptions: {},
 
       vitePlugins: [
-        ['@intlify/vite-plugin-vue-i18n', {
-          // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-          // compositionOnly: false,
+        [
+          '@intlify/vite-plugin-vue-i18n',
+          {
+            // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+            // compositionOnly: false,
 
-          // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
-          // you need to set `runtimeOnly: false`
-          // runtimeOnly: false,
+            // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
+            // you need to set `runtimeOnly: false`
+            // runtimeOnly: false,
 
-          // you need to set i18n resource including paths !
-          include: path.resolve(__dirname, './src/i18n/**')
-        }],
-        ['vite-plugin-checker', {
-          vueTsc: {
-            tsconfigPath: 'tsconfig.vue-tsc.json'
+            // you need to set i18n resource including paths !
+            include: path.resolve(__dirname, './src/i18n/**'),
           },
-          eslint: {
-            lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"'
-          }
-        }, { server: false }]
-      ]
+        ],
+        [
+          'vite-plugin-checker',
+          {
+            vueTsc: {
+              tsconfigPath: 'tsconfig.vue-tsc.json',
+            },
+            eslint: {
+              lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"',
+            },
+          },
+          { server: false },
+        ],
+      ],
     },
 
     // Full list of options: https://legacy-app.quasar.dev/quasar-cli-vite-v1/quasar-config-file#devserver
     devServer: {
       // https: true
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://legacy-app.quasar.dev/quasar-cli-vite-v1/quasar-config-file#framework
@@ -146,11 +147,11 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
-        'render' // keep this as last one
-      ]
+        'render', // keep this as last one
+      ],
     },
 
     // https://legacy-app.quasar.dev/quasar-cli-vite-v1/developing-pwa/configuring-pwa
@@ -174,7 +175,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://legacy-app.quasar.dev/quasar-cli-vite-v1/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     // Full list of options: https://legacy-app.quasar.dev/quasar-cli-vite-v1/developing-electron-apps/configuring-electron
@@ -188,13 +189,11 @@ module.exports = configure(function (/* ctx */) {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -202,18 +201,16 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'homepage'
-      }
+        appId: 'homepage',
+      },
     },
 
     // Full list of options: https://legacy-app.quasar.dev/quasar-cli-vite-v1/developing-browser-extensions/configuring-bex
     bex: {
-      contentScripts: [
-        'my-content-script'
-      ],
+      contentScripts: ['my-content-script'],
 
       // extendBexScriptsConf (esbuildConf) {}
       // extendBexManifestJson (json) {}
-    }
-  }
+    },
+  };
 });
---
+++ Homepage/src/boot/defaults.ts
@@ -0,0 +1,31 @@
+import { QBtn } from 'quasar';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const setDefault = (comp: any, key: string, defaultValue: any) => {
+  if (comp.props && comp.props[key]) {
+    if (comp.props[key].type) {
+      comp.props[key].default = defaultValue;
+    } else {
+      comp.props[key] = { type: comp.props[key], default: defaultValue };
+    }
+  } else {
+    // Optionally log or handle the case where the prop does not exist
+    console.warn(`Prop '${key}' does not exist on component`, comp);
+  }
+};
+
+/*
+const setMulti = (
+  comps: Component | Component[],
+  keyvals: Record<string, any>
+) => {
+  (Array.isArray(comps) ? comps : [comps]).forEach((comp) => {
+    Object.entries(keyvals).forEach(([key, val]) => {
+      setDefault(comp, key, val);
+    });
+  });
+  return keyvals;
+};
+*/
+
+setDefault(QBtn, 'textColor', 'black');
